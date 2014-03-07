2042035706b8056a0a8ea3d21bf154de3c2189c7
Nat <nat.wrw@gmail.com>
2014-03-07 18:47:06 +0700

    Add METASHEET

--- conf.py
+++ conf.py
@@ -7,3 +7,4 @@ class Config(object):
     GDOCS_USERNAME = os.environ.get('GDOCS_USERNAME') or "nat.wrw@gmail.com"
     GDOCS_PASSWORD = os.environ.get('GDOCS_PASSWORD') or "yxafbjpgqidckxpc"
     GDOCS_FALLBACK_SHEET_KEY = os.environ.get("GDOCS_FALLBACK_SHEET_KEY") or "0Av03mDBecdQEdHlMWXZqV01MV3RZb0J0ejJIaFRFbkE"
+    GDOCS_KEYS_OF_METASHEET = os.environ.get('GDOCS_KEYS_OF_METASHEET') or "0Av03mDBecdQEdHlMWXZqV01MV3RZb0J0ejJIaFRFbkE"
