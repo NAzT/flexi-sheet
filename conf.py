111f2d0a81390d9a7823293b984f0f4b1dc13a78
Nat <nat.wrw@gmail.com>
2014-03-07 18:40:26 +0700

    Change config

--- conf.py
+++ conf.py
@@ -2,12 +2,8 @@ import os
 
 class Config(object):
     DEBUG = True
-    TESTING = False
     LOG_LEVEL = os.environ.get('LOG_LEVEL', 'DEBUG')
-    FBAPI_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-    FBAPI_APP_SECRET = os.environ.get('FACEBOOK_SECRET')
-    FBAPI_SCOPE = ['user_likes', 'user_photos', 'user_photo_video_tags']
 
-    GDOCS_USERNAME = os.environ.get('GDOCS_USERNAME')
-    GDOCS_PASSWORD = os.environ.get('GDOCS_PASSWORD')
-    GDOCS_FALLBACK_SHEET_KEY = os.environ.get("GDOCS_FALLBACK_SHEET_KEY")
+    GDOCS_USERNAME = os.environ.get('GDOCS_USERNAME') or "nat.wrw@gmail.com"
+    GDOCS_PASSWORD = os.environ.get('GDOCS_PASSWORD') or "yxafbjpgqidckxpc"
+    GDOCS_FALLBACK_SHEET_KEY = os.environ.get("GDOCS_FALLBACK_SHEET_KEY") or "0Av03mDBecdQEdHlMWXZqV01MV3RZb0J0ejJIaFRFbkE"
