556cc9cf8bd6afc3a649b1fe5bdf6c951bfda946
Nat <nat.wrw@gmail.com>
2014-03-13 20:01:34 +0700

    Remove login

--- conf.py
+++ conf.py
@@ -4,6 +4,6 @@ class Config(object):
     DEBUG            = True
     LOG_LEVEL        = os.environ.get('LOG_LEVEL', 'DEBUG')
     APP_VERSION      = 0.2
-    GDOCS_USERNAME   = os.environ.get('GDOCS_USERNAME')   or "nat.wrw@gmail.com"
-    GDOCS_PASSWORD   = os.environ.get('GDOCS_PASSWORD')   or "yxafbjpgqidckxpc"
-    GDOCS_META_SHEET = os.environ.get('GDOCS_META_SHEET') or "0Av03mDBecdQEdHlMWXZqV01MV3RZb0J0ejJIaFRFbkE"
+    GDOCS_USERNAME   = os.environ.get('GDOCS_USERNAME')   or ""
+    GDOCS_PASSWORD   = os.environ.get('GDOCS_PASSWORD')   or ""
+    GDOCS_META_SHEET = os.environ.get('GDOCS_META_SHEET') or ""
